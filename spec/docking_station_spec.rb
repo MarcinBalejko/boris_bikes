0c241554c1dd5c12c2809f253cac6e8f851e3fe4
MarcinBalejko <mbbm9@o2.pl>
2019-05-02 22:05:06 +0800

    Added test to dock method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,5 +10,8 @@ describe DockingStation do
         bike = subject.release_bike
         expect(bike).to be_working
     end
+describe DockingStation do
+    it {is_expected.to respond_to(:dock).with(1).argument}
+end
 end
 end
