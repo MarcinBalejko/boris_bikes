d748721f402f359c41089818d5960f913aab3261
MarcinBalejko <mbbm9@o2.pl>
2019-05-05 02:33:04 +0800

    :white_check_mark: Default capacity constant

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,7 @@
 require_relative 'bike'
 class DockingStation
     attr_reader :bike
+    DEFAULT_CAPACITY = 20
     def initialize
         @bikes = []
     end
@@ -15,7 +16,7 @@ class DockingStation
 
     private
     def full?
-        @bikes.count >= 20
+        @bikes.count >= DEFAULT_CAPACITY
     end
     def empty?
         @bikes.empty?
