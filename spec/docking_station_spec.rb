037443ddc542ba1fa431d90e7c6218ed637dee21
MarcinBalejko <mbbm9@o2.pl>
2019-05-04 22:26:15 +0800

    :white_check_mark: Raised bike capacity to 20

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -39,7 +39,7 @@ end
 describe DockingStation do
     describe '#dock' do 
         it 'raises an error when full' do
-        subject.dock(Bike.new)
+        20.times {subject.dock Bike.new}
         expect{subject.dock Bike.new}.to raise_error 'Docking station full'
         end
     end
