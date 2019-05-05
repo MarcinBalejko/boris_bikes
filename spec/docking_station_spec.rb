57e66ab16ac0daf90dad4f918462d259cdb29cc9
MarcinBalejko <mbbm9@o2.pl>
2019-05-06 02:31:38 +0800

    :fire: Refatored tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,12 +2,10 @@ require './lib/docking_station.rb'
 
 describe DockingStation do
 
-    subject {DockingStation.new}
     let(:bike) {Bike.new}
 
-    it "Releases a bike" do
-        docking_station = DockingStation.new
-        expect(docking_station).to respond_to(:release_bike)
+    it "releases a bike" do
+        expect(subject).to respond_to(:release_bike)
     end
 
     it {is_expected.to respond_to(:dock).with(1).argument}
@@ -15,18 +13,15 @@ describe DockingStation do
     it {is_expected.to respond_to(:release_bike)}
 
     it "releases working bikes" do
-        bike = Bike.new
         subject.dock(bike)
         expect(bike).to be_working
     end
 
     it 'docks a bike' do
-        bike = Bike.new
         expect(subject.dock(bike)[0]).to eq bike
     end    
 
     it 'releases bike' do
-        bike = Bike.new
         subject.dock(bike)
         expect(subject.release_bike).to eq bike
     end
@@ -45,12 +40,10 @@ describe DockingStation do
     end
 
     it 'defaults capacity' do
-        described_class::DEFAULT_CAPACITY.times do
-            subject.dock(bike)
-        end
+        described_class::DEFAULT_CAPACITY.times { subject.dock(bike) }
         expect{subject.dock(bike)}.to raise_error 'Docking station full'
     end
-
+ 
     it 'keeps the broken bike' do 
         bike.report_broken
         subject.dock(bike)
