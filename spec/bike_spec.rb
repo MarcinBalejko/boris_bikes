e7e8d1b071c524ea25f695549e04119fe8963837
MarcinBalejko <mbbm9@o2.pl>
2019-05-05 23:36:13 +0800

    :white_check_mark: Bike  report_broken/broken

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,6 +2,9 @@ require './lib/bike'
 describe Bike do
     it {is_expected.to respond_to :working?}
 end
-#describe Bike do
-    #it {is_expected.to respond_to :return}
-#end
+describe Bike do
+    it 'it can be reported broken' do
+    subject.report_broken
+    expect(subject).to be_broken
+    end
+end
