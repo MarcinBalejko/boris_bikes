83c2797cf939820d145b379b73ac62afd8f763d6
MarcinBalejko <mbbm9@o2.pl>
2019-05-08 02:46:02 +0800

    :recycle: Van class

--- lib/van.rb
+++ lib/van.rb
@@ -1,10 +1,32 @@
 require_relative 'bike'
+
 class Van
-    attr_reader :broken_bikes
-    def initialize
-        @broken_bikes = []
+    attr_reader :vancapacity
+    VAN_CAPACITY = 20
+    def initialize(vancapacity= VAN_CAPACITY)
+        @vancapacity = vancapacity
+        @broken_bikes = [] 
+    end
+    def release_broken
+        fail 'No bikes available' if empty? || @broken_bikes[-1].broken? == false
+        @broken_bikes.pop
+    end
+    def release_working
+        fail 'No bikes available' if empty? || @broken_bikes[-1].broken? == true
+        @broken_bikes.pop
     end
     def take_broken(bike)
+        fail 'Docking station full' if full?
+        fail 'No broken bikes' if  @broken_bikes[-1].broken? == false
         @broken_bikes << bike
+    end
+
+    private
+    attr_reader :broken_bikes
+    def full?
+        @broken_bikes.count >= @vancapacity
+    end
+    def empty?
+        @broken_bikes.empty?
     end
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -2,18 +2,15 @@ require './lib/van.rb'
 
 describe Van do
 
-    let(:van) {Van.new}
+    let(:bike) {Bike.new}
 
     it 'reacts to take_broken method' do
     expect(subject).to respond_to(:take_broken)
     end
 
-    it 'takes broken bike' do
-        bike = Bike.new
-        expect(van.take_broken(bike)[0]).to eq bike
+    it 'leaves ok bike' do
+        bike = double(:bike, broken?: true)
+        expect{subject.take_broken(bike)}.to raise_error 'No broken bikes'
     end
-
-
-
 
 end
