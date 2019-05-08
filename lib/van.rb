a18c693d3c8f4720ffe0fc772d057343a462dfdb
MarcinBalejko <mbbm9@o2.pl>
2019-05-09 01:12:33 +0800

    :bug: take_broken method fixed

--- lib/van.rb
+++ lib/van.rb
@@ -2,13 +2,21 @@ require_relative 'bike'
 
 class Van
     attr_reader :broken_bikes, :van_capacity
-    DEFAULT_VAN_CAPACITY = 20
+    DEFAULT_VAN_CAPACITY = 3
     def initialize(van_capacity= DEFAULT_VAN_CAPACITY)
         @van_capacity = van_capacity
         @broken_bikes = [] 
     end
     def take_broken(bike)
         @broken_bikes << bike
-        fail 'No broken bikes' if  @broken_bikes[-1].broken? != false
+        fail 'No broken bikes' if  @broken_bikes[-1].broken? != true
+        fail 'Van is full' if full?
     end
+
+    private
+    attr_reader :bikes
+    def full?
+        @broken_bikes.count >= @van_capacity
+    end
+
 end
