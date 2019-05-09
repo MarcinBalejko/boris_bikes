427d71a6a38b868546d6d27cf89b5e396d1f4d1b
MarcinBalejko <mbbm9@o2.pl>
2019-05-09 19:28:44 +0800

    :white_check_mark: Garage default capacity

--- lib/garage.rb
+++ lib/garage.rb
@@ -2,13 +2,27 @@ require_relative 'van'
 require_relative 'bike'
 
 class Garage
-    attr_reader :bikes_to_repair
-    def initialize
+    attr_reader :bikes_to_repair, :garage_capacity
+    DEFAULT_GARAGE_CAPACITY = 3
+    def initialize(garage_capacity= DEFAULT_GARAGE_CAPACITY)
         @bikes_to_repair = []
+        @garage_capacity = garage_capacity
     end
     def receive_broken(bike)
         @bikes_to_repair << bike
 
     end
+    def repair
+        @bikes_to_repair.each do |x|
+            @broken = false  
+        end
+    end
+    def release_working_bike
+        fail 'No bikes available' if empty? || @bikes_to_repair[-1].broken? != false
+        @bikes_to_repair.pop
+    end
+
+
+    
 end
 
--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -2,7 +2,7 @@ require './lib/garage.rb'
 
 describe Garage do
 
-    
+    let(:bike) {Bike.new}
 
     it 'reacts to receive_broken method' do
     expect(subject).to respond_to(:receive_broken)
@@ -13,4 +13,15 @@ describe Garage do
         bike.report_broken
         expect(subject.receive_broken(bike)[-1]).to eq bike
     end
+
+    #it 'repairs bikes' do
+    #bike = double(:bike, broken?: true)
+    #subject.receive_broken bike
+    #subject.repair
+    #subject.
+    #end
+
+    it 'has a default capacity' do
+        expect(subject.garage_capacity).to eq Garage::DEFAULT_GARAGE_CAPACITY
+        end
 end
